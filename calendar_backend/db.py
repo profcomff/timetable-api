eae0040dc41bc26dfb3ed7c886f55a07a417e302
Mikhail <87110754+mixx3@users.noreply.github.com>
2022-04-27 00:24:15 +0300

    timetable dataclass added
    for proper service.py work, update db.py (modified credentials + added timetable dataclass)

--- calendar_backend/db.py
+++ calendar_backend/db.py
@@ -1,9 +1,12 @@
 """Database common classes and methods
 """
+import os
 import re
 from datetime import datetime
 from typing import Any
+from sqlalchemy import *
 
+import psycopg2
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
@@ -27,15 +30,25 @@ class BaseModel:
 
 class Credentials(BaseModel):
     """User credentials"""
-
     id = Column(Integer, primary_key=True)
-    code = Column(String, nullable=False)
-    scope = Column(JSON, nullable=False)
     group = Column(String, nullable=False)
-    prompt = Column(String, nullable=False)
-    authuser = Column(Integer, nullable=False)
-    hd = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    scope = Column(JSON, nullable=False)
+    token = Column(JSON, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(
-        DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow
-    )
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class Timetable(BaseModel):
+    start = Column(String, nullable=False)
+    end = Column(String, nullable=False)
+    odd = Column(Boolean, nullable=False)
+    even = Column(Boolean, nullable=False)
+    type = Column(String, nullable=False)
+    weekday = Column(Integer, nullable=False)
+    num = Column(Integer, nullable=False)
+    group = Column(String, nullable=False)
+    id = Column(String, nullable=False, primary_key=True)
+    subject = Column(String, nullable=False)
+    place = Column(String, nullable=True)
+    teacher = Column(String, nullable=True)
