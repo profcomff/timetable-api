4e0ac5f271cee185b3675c7079848632ed060166
Mikhail <mmikee00800@gmail.com>
2022-04-27 15:14:51 +0300

    Revert "Update settings.py"
    This reverts commit a7e361fd4699e175ebc1b3defe7f5331d595e226.

--- calendar_backend/settings.py
+++ calendar_backend/settings.py
@@ -7,13 +7,13 @@ from typing import List, Optional
 class Settings(BaseSettings):
     """Application settings"""
 
-    DB_DSN: PostgresDsn = os.getenv('FFPOSTGRES_DSN')
+    DB_DSN: PostgresDsn 
     GOOGLE_CREDS: Json
     APP_URL: Optional[AnyHttpUrl] = None
     REDIRECT_URL: AnyHttpUrl = "https://www.profcomff.com"
     GROUPS: List[str] = ["101", "102"]
     TIMETABLE_NAME: str = 'timetable'
-    #ICS_PATH: str
+    ICS_PATH: str
 
     class Config:
         """Pydantic BaseSettings config"""
