83085480042105f1ce4c3e732a9303bb76344c71
semen603089 <semen603089@mail.ru>
2022-08-11 23:58:47 +0300

    logs

--- calendar_backend/routes/cud.py
+++ calendar_backend/routes/cud.py
@@ -110,15 +110,17 @@ async def http_patch_room(room_pydantic: Room, new_name: str | None = None) -> R
 
 @cud_router.patch("/patch/group/", response_model=Group)
 async def http_patch_group(group_pydantic: Group, new_number: str | None = None, new_name: str | None = None) -> Group:
+    logger.debug(f"Patching {group_pydantic}")
     try:
         group = await utils.get_group_by_name(group_pydantic.number, session=db.session)
         return Group.from_orm(
             await utils.update_group(group, session=db.session, new_name=new_name, new_number=new_number)
         )
-    except exceptions.NoGroupFoundError:
+    except exceptions.NoGroupFoundError as e:
+        logger.info(f"Patching {group_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="No group found")
     except ValueError as e:
-        logger.info(f"Failed to patch room")
+        logger.info(f"Patching {group_pydantic} failed with error: {e}")
         raise HTTPException(status_code=500, detail=e)
 
 
@@ -129,6 +131,7 @@ async def http_patch_lecturer(
     new_middle_name: str | None = None,
     new_last_name: str | None = None,
 ) -> Lecturer:
+    logger.debug(f"Patching {lecturer_pydantic}")
     try:
         lecturer = await utils.get_lecturer_by_name(
             first_name=lecturer_pydantic.first_name,
@@ -145,9 +148,11 @@ async def http_patch_lecturer(
                 new_last_name=new_last_name,
             )
         )
-    except exceptions.NoTeacherFoundError:
+    except exceptions.NoTeacherFoundError as e:
+        logger.info(f"Patching {lecturer_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="No lecturer found")
     except ValueError as e:
+        logger.info(f"Patching {lecturer_pydantic} failed with error: {e}")
         raise HTTPException(status_code=500, detail=e)
 
 
@@ -161,6 +166,7 @@ async def http_patch_lesson(
     new_start_ts: datetime.datetime | None = None,
     new_end_ts: datetime.datetime | None = None,
 ) -> Lesson:
+    logger.debug(f"Patching {lesson_pydantic}")
     try:
         lesson = await utils.get_lesson(
             lesson_pydantic.name,
@@ -183,41 +189,51 @@ async def http_patch_lesson(
                 new_room=new_room,
             )
         )
-    except exceptions.TimetableNotFound:
+    except exceptions.TimetableNotFound as e:
+        logger.info(f"Patching {lesson_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="Lesson not found")
-
+    except ValueError as e:
+        logger.info(f"Patching {lesson_pydantic} failed with error: {e}")
+        raise HTTPException(status_code=500, detail=e)
 
 @cud_router.delete("/delete/room/")
 async def http_delete_room(room_pydantic: Room) -> None:
+    logger.debug(f"Deleting {room_pydantic}")
     try:
         room = await utils.get_room_by_name(room_pydantic.name, session=db.session)
         return await utils.delete_room(room, session=db.session)
-    except exceptions.NoAudienceFoundError:
+    except exceptions.NoAudienceFoundError as e:
+        logger.info(f"Deleting {room_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="No room found")
 
 
 @cud_router.delete("/delete/lecturer/")
 async def http_delete_lecturer(lecturer_pydantic: Lecturer) -> None:
+    logger.debug(f"Deleting {lecturer_pydantic}")
     try:
         lecture = await utils.get_lecturer_by_name(
             lecturer_pydantic.first_name, lecturer_pydantic.middle_name, lecturer_pydantic.last_name, session=db.session
         )
         return await utils.delete_lecturer(lecture, session=db.session)
-    except exceptions.NoTeacherFoundError:
+    except exceptions.NoTeacherFoundError as e:
+        logger.info(f"Deleting {lecturer_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="No lecturer found")
 
 
 @cud_router.delete("/delete/group/")
 async def http_delete_group(group_pydantic: Group) -> None:
+    logger.debug(f"Deleting {group_pydantic}")
     try:
         group = await utils.get_group_by_name(group_pydantic.number, session=db.session)
         return await utils.delete_group(group, session=db.session)
-    except exceptions.NoGroupFoundError:
+    except exceptions.NoGroupFoundError as e:
+        logger.info(f"Deleting {group_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="No group found")
 
 
 @cud_router.delete("/delete/lesson/")
 async def http_delete_lesson(lesson_pydantic: Lesson) -> None:
+    logger.debug(f"Deleting {lesson_pydantic}")
     try:
         lesson = await utils.get_lesson(
             lesson_pydantic.name,
@@ -229,5 +245,6 @@ async def http_delete_lesson(lesson_pydantic: Lesson) -> None:
             session=db.session,
         )
         return await utils.delete_lesson(lesson, session=db.session)
-    except exceptions.TimetableNotFound:
+    except exceptions.TimetableNotFound as e:
+        logger.info(f"Deleting {lesson_pydantic} failed with error: {e}")
         raise HTTPException(status_code=404, detail="Lesson not found")
