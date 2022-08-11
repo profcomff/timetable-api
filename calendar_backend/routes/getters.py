3dae5fef145484d5a81a284bcd4a7af864ba9c2e
semen603089 <semen603089@mail.ru>
2022-08-11 14:58:34 +0300

    getters

--- calendar_backend/routes/getters.py
+++ calendar_backend/routes/getters.py
@@ -93,3 +93,34 @@ async def download_ics_file(group: str = Query(..., description="Group number"))
             return FileResponse(
                 await calendar_backend.methods.list_calendar.create_user_calendar_file(user_calendar, group)
             )
+
+
+@getters_router.get("/group/future/{group_number}", response_model=Group)
+async def http_get_group(group_number: str) -> Group:
+    try:
+        return Group.from_orm(utils.get_group_by_name(group_number, session=db.session))
+    except exceptions.NoGroupFoundError:
+        raise HTTPException(status_code=404, detail="No group found")
+    except ValueError as e:
+        logger.info(f"Failed to get group {group_number}, error {e} occurred")
+
+
+@getters_router.get("/lecturer/future", response_model=Lecturer)
+async def http_get_lecturer(lecturer: Lecturer) -> Lecturer:
+    try:
+        return Lecturer.from_orm(utils.get_lecturer_by_name(**lecturer.dict(), session=db.session))
+    except exceptions.TeacherTimetableNotFound:
+        raise HTTPException(status_code=404, detail="Lecturer not found")
+    except ValueError as e:
+        logger.info(f"Failed to get lecturer {lecturer}, error {e} occurred")
+
+
+@getters_router.get("/room/future", response_model=Room)
+async def http_get_room(room_name: str) -> Room:
+    try:
+        return Room.from_orm(utils.get_room_by_name(room_name, session=db.session))
+    except exceptions.AudienceTimetableNotFound:
+        raise HTTPException(status_code=404, detail="Room not found")
+    except ValueError as e:
+        logger.info(f"Failed to get room {room_name}, error {e} occurred")
+
