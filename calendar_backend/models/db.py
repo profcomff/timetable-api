ad016035872cf2aeb766a3ff26400af65f490673
semen603089 <semen603089@mail.ru>
2022-08-19 00:32:01 +0300

    db update

--- calendar_backend/models/db.py
+++ calendar_backend/models/db.py
@@ -39,7 +39,7 @@ class Room(Base):
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     direction = sqlalchemy.Column(sqlalchemy.Enum("North", "South", name="Directions"), nullable=True)
     lessons: list[Lesson] = sqlalchemy.orm.relationship(
-        "Lesson", foreign_keys="Lesson.room_id", order_by="(Lesson.start_ts)"
+        "Lesson", back_populates="room", secondary="lessons_rooms", order_by="(Lesson.start_ts)"
     )
 
     def __repr__(self):
@@ -52,7 +52,7 @@ class Lecturer(Base):
     middle_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     last_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     lessons: list[Lesson] = sqlalchemy.orm.relationship(
-        "Lesson", foreign_keys="Lesson.lecturer_id", order_by="(Lesson.start_ts)"
+        "Lesson", secondary="lessons_lecturers", order_by="(Lesson.start_ts)", back_populates="lecturer"
     )
 
     def __repr__(self):
@@ -74,16 +74,18 @@ class Group(Base):
 class Lesson(Base):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    room_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("room.id"))
     group_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("group.id"))
-    lecturer_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("lecturer.id"))
     start_ts = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
     end_ts = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
 
-    room: Room = sqlalchemy.orm.relationship("Room", foreign_keys="Lesson.room_id", back_populates="lessons")
-    group: Group = sqlalchemy.orm.relationship("Group", foreign_keys="Lesson.group_id", back_populates="lessons")
+    room: Room = sqlalchemy.orm.relationship(
+        "Room", back_populates="lessons", secondary="lessons_rooms"
+    )
+    group: Group = sqlalchemy.orm.relationship(
+        "Group", foreign_keys="Lesson.group_id", back_populates="lessons"
+    )
     lecturer: Lecturer = sqlalchemy.orm.relationship(
-        "Lecturer", foreign_keys="Lesson.lecturer_id", back_populates="lessons"
+        "Lecturer", back_populates="lessons", secondary="lessons_lecturers"
     )
 
     def __repr__(self):
@@ -92,3 +94,15 @@ class Lesson(Base):
             f" room={self.room}, group={self.group},"
             f" lecturer={self.lecturer}, start_ts={self.start_ts}, end_ts={self.end_ts})"
         )
+
+
+class LessonsLecturers(Base):
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    lesson_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("lesson.id"))
+    lecturer_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("lecturer.id"))
+
+
+class LessonsRooms(Base):
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    lesson_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("lesson.id"))
+    room_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("room.id"))
