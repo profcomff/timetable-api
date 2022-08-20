9dd5df4ad62a983109e6d704cfbc2432b289d31c
semen603089 <semen603089@mail.ru>
2022-08-20 22:14:09 +0300

    Update timetable.py

--- calendar_backend/routes/timetable.py
+++ calendar_backend/routes/timetable.py
@@ -16,7 +16,7 @@ settings = get_settings()
 logger = logging.getLogger(__name__)
 
 
-@timetable_router.get("/by_group/{group_num}", response_model=list[Event])
+@timetable_router.get("/by_group/{group_num}", response_model=list[Event], deprecated=True)
 async def get_timetable_by_group(
     group_num: str, start: datetime.date, end: datetime.date, format: Literal['ics', 'json'] = 'json'
 ) -> list[Event] | FileResponse:
@@ -28,17 +28,3 @@ async def get_timetable_by_group(
             logger.debug(f"Getting lessons for group {group_num}")
             group = await utils.get_list_groups(db.session, filter_group_number=group_num)
             return [Event.from_orm(row) for row in await utils.get_group_lessons_in_daterange(group=group, date_start=start, date_end=end)]
-
-
-@timetable_router.get("/by_room/{room_num}", response_model=list[Event])
-async def get_timetable_by_room(room_num: str, start: datetime.date, end: datetime.date) -> list[Event]:
-    logger.debug(f"Getting lessons for room {room_num}")
-    room = await utils.get_list_rooms(db.session, room_num)
-    return [Event.from_orm(row) for row in await utils.get_room_lessons_in_daterange(room, start, end)]
-
-
-@timetable_router.get("/by_lecturer", response_model=list[list[Event]])
-async def get_timetable_by_lecturer(first_name: str, middle_name: str, last_name: str, start: datetime.date, end: datetime.date) -> list[list[Event]]:
-    logger.debug(f"Getting lessons for lecturer {first_name} {middle_name} {last_name}")
-    lecturer = await utils.get_list_lecturers(db.session, first_name, middle_name, last_name)
-    return [await utils.get_lecturer_lessons_in_daterange(row, start, end) for row in lecturer]
