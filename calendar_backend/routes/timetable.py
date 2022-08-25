ae4ba751f57e7173dfce6ab19dd3f306d2c933f7
semen603089 <semen603089@mail.ru>
2022-08-25 04:31:23 +0300

    review

--- calendar_backend/routes/timetable.py
+++ calendar_backend/routes/timetable.py
@@ -18,13 +18,13 @@ logger = logging.getLogger(__name__)
 
 @timetable_router.get("/by_group/{group_num}", response_model=list[base.Event], deprecated=True)
 async def get_timetable_by_group(
-    group_num: str, start: datetime.date, end: datetime.date, format: Literal['ics', 'json'] = 'json'
+    group_id: int, start: datetime.date, end: datetime.date, format: Literal['ics', 'json'] = 'json'
 ) -> list[base.Event] | FileResponse:
-    match format:
-        case 'ics':
-            logger.debug(f"Downloading .ics file for {group_num} calendar...")
-            return await list_calendar.create_ics(group_num, start, end, db.session)
-        case 'json':
-            logger.debug(f"Getting lessons for group {group_num}")
-            group, _ = await utils.get_list_groups(db.session, group_num)
-            return [Event.from_orm(row) for row in await utils.get_group_lessons_in_daterange(group=group, date_start=start, date_end=end)]
+    raise NotImplementedError()
+    # match format:
+    #     case 'ics':
+    #         logger.debug(f"Downloading .ics file for {group_id} calendar...")
+    #         return await list_calendar.create_ics(group_id, start, end, db.session)
+    #     case 'json':
+    #         logger.debug(f"Getting lessons for group {group_id}")
+    #         return await utils.get_group_lessons_in_daterange(await utils.get_group_by_id(group_id, db.session), start, end)
