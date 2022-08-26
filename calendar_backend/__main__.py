d2d407495cf5ea3d2f198a49fdc1f5163ff70247
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2022-08-26 21:37:36 +0300

    Revert logs

--- calendar_backend/__main__.py
+++ calendar_backend/__main__.py
@@ -4,7 +4,12 @@ import uvicorn
 
 from calendar_backend.routes import app
 
+logging.basicConfig(
+    filename=f'logger_{__name__}.log',
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    logger.info("hi")
     uvicorn.run(app)
--- calendar_backend/methods/auth.py
+++ calendar_backend/methods/auth.py
@@ -1,8 +1,10 @@
+import logging
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 from calendar_backend.settings import get_settings
 
+logger = logging.getLogger(__name__)
 settings = get_settings()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -29,4 +31,5 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    logger.info("User auth successfull %s", user.username)
     return user.username
