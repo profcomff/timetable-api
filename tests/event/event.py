6b546282ce06d8a078a6e4051e4c11bf0b8f6418
semen603089 <semen603089@mail.ru>
2022-08-27 01:28:32 +0300

    init

--- tests/event/event.py
+++ tests/event/event.py
@@ -1,5 +1,21 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
-from calendar_backend.models import Group
+from calendar_backend.models import Lesson
 
 RESOURCE = "/timetable/event/"
+
+
+def test_create(client_auth: TestClient, dbsession: Session):
+    pass
+
+
+def test_read():
+    pass
+
+
+def test_delete():
+    pass
+
+
+def test_update():
+    pass
