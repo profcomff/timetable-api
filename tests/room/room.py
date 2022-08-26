132b190ecac8d3a87ffcb3bc9816acb38b9488e0
semen603089 <semen603089@mail.ru>
2022-08-27 02:24:33 +0300

    room upd

--- tests/room/room.py
+++ tests/room/room.py
@@ -101,5 +101,57 @@ def test_delete(client_auth: TestClient, dbsession: Session):
     dbsession.commit()
 
 
-def test_update():
-    pass
+def test_update_all(client_auth: TestClient, dbsession: Session):
+    # Create
+    request_obj = {
+        "name": "" + datetime.datetime.utcnow().isoformat(),
+        "direction": "North"
+    }
+    response = client_auth.post(RESOURCE, json=request_obj)
+    assert response.ok, response.json()
+    response_obj = response.json()
+    assert response_obj["name"] == request_obj["name"]
+    assert response_obj["direction"] == request_obj["direction"]
+    id_ = response_obj['id']
+
+    # Read
+    response = client_auth.get(RESOURCE+f"{id_}/")
+    assert response.ok, response.json()
+    response_obj = response.json()
+    assert response_obj["name"] == request_obj["name"]
+    assert response_obj["direction"] == request_obj["direction"]
+
+    # Update
+    request_obj_2 = {
+        "name": "hjbhb" + datetime.datetime.utcnow().isoformat(),
+        "direction": "North"
+    }
+    client_auth.patch(RESOURCE+f"{id_}/", json=request_obj_2)
+    response = client_auth.get(RESOURCE+f"{id_}/")
+    assert response.ok, response.json()
+    response_obj = response.json()
+    assert response_obj["name"] == request_obj_2["name"]
+    assert response_obj["direction"] == request_obj_2["direction"]
+
+    # Read all
+    response = client_auth.get(RESOURCE, params={"limit": 0}, json=request_obj)
+    assert response.ok
+    for item in response.json()["items"]:
+        if item["id"] == id_:
+            assert item["name"] == request_obj_2["name"]
+            assert item["direction"] == request_obj_2["direction"]
+
+    # Ok reverse
+    assert response.ok, response.json()
+    response_obj = response.json()
+    assert response_obj["name"] == request_obj["name"]
+    assert response_obj["direction"] == request_obj["direction"]
+
+    # Ok db
+    response_model: Room = dbsession.query(Room).get(response_obj["id"])
+    assert response_model.name == request_obj["name"]
+    assert response_model.direction == request_obj["direction"]
+
+    # Clear db
+    dbsession.delete(response_model)
+    dbsession.commit()
