7d10422140706c6fa6761503b31c85d0cf797b1f
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2022-08-27 05:16:11 +0300

    Base model update

--- calendar_backend/models/base.py
+++ calendar_backend/models/base.py
@@ -1,7 +1,12 @@
+from __future__ import annotations
+
 import re
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.orm import Session
+
 from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import Query, Session
+
+from calendar_backend.exceptions import ObjectNotFound
 
 
 @as_declarative()
@@ -17,29 +22,46 @@ class DeclarativeBase:
 
 
 class BaseDbModel(DeclarativeBase):
+    __abstract__ = True
+
     @classmethod
-    def get_all(cls, *, with_deleted=False, session: Session):
-        """Get all objects with soft deletes
-        """
+    def create(cls, *, session: Session, **kwargs) -> BaseDbModel:
+        obj = cls(**kwargs)
+        session.add(obj)
+        session.flush()
+        return obj
+
+    @classmethod
+    def get_all(cls, limit: int | None = 100, offset: int = 0, *, with_deleted: bool = False, session: Session) -> Query[BaseDbModel]:
+        """Get all objects with soft deletes"""
         objs = session.query(cls)
         if not with_deleted and hasattr(cls, "is_deleted"):
             objs = objs.filter(cls.is_deleted)
-        return objs
+        return objs.offset(offset).limit(limit)
 
     @classmethod
-    def get(cls, id: int, *, with_deleted=False, session: Session):
-        """Get object with soft deletes
-        """
+    def get(cls, id: int, *, with_deleted=False, session: Session) -> BaseDbModel:
+        """Get object with soft deletes"""
         objs = session.query(cls)
         if not with_deleted and hasattr(cls, "is_deleted"):
             objs = objs.filter(cls.is_deleted)
         try:
             return objs.get(id)
         except NoResultFound:
-            return None
+            raise ObjectNotFound(cls, id)
 
-    def delete(self):
-        """Soft delete object if possible, else hard delete
-        """
-        if hasattr(self, "is_deleted"):
-            self.is_deleted = True
+    @classmethod
+    def update(cls, id: int, *, session: Session, **kwargs) -> BaseDbModel:
+        obj: cls = cls.get(id, session=session)
+        for k, v in kwargs.items():
+            setattr(obj, k, v)
+        session.flush()
+        return obj
+
+    @classmethod
+    def delete(cls, id: int, *, session: Session) -> None:
+        """Soft delete object if possible, else hard delete"""
+        obj = cls.get(id, session=session)
+        if hasattr(obj, "is_deleted"):
+            obj.is_deleted = True
+        session.flush()
