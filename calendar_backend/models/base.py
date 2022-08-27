1c7cd75046f2f6b6d0415daac09c456b68b5d5fb
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2022-08-27 04:29:51 +0300

    Methods for soft deletes

--- calendar_backend/models/base.py
+++ calendar_backend/models/base.py
@@ -1,21 +1,12 @@
 import re
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.orm import Query
-
-
-class CustomQuery(Query):
-    def __new__(cls, *args, **kwargs):
-        if args and hasattr(args[0][0], "is_deleted"):
-            return Query(*args, **kwargs).filter_by(is_deleted=False)
-        else:
-            return object.__new__(cls)
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
 
 
 @as_declarative()
 class Base:
     """Base class for all database entities"""
-
-    query_class = CustomQuery
 
     @declared_attr
     def __tablename__(cls) -> str:  # pylint: disable=no-self-argument
@@ -23,3 +14,30 @@ class Base:
         Convert CamelCase class name to snake_case db table name.
         """
         return re.sub(r"(?<!^)(?=[A-Z])", "_", cls.__name__).lower()
+
+    @classmethod
+    def get_all(cls, *, with_deleted=False, session: Session):
+        """Get all objects with soft deletes
+        """
+        objs = session.query(cls)
+        if not with_deleted and hasattr(cls, "is_deleted"):
+            objs = objs.filter(cls.is_deleted)
+        return objs
+
+    @classmethod
+    def get(cls, id: int, *, with_deleted=False, session: Session):
+        """Get object with soft deletes
+        """
+        objs = session.query(cls)
+        if not with_deleted and hasattr(cls, "is_deleted"):
+            objs = objs.filter(cls.is_deleted)
+        try:
+            return objs.get(id)
+        except NoResultFound:
+            return None
+
+    def delete(self, id: int):
+        """Soft delete object if possible, else hard delete
+        """
+        if hasattr(self, "is_deleted"):
+            self.is_deleted = True
