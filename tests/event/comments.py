37b970f39abfbaa9ef3588875690ab0d3fd95424
semen603089 <semen603089@mail.ru>
2022-09-12 19:18:30 +0300

    review

--- tests/event/comments.py
+++ tests/event/comments.py
@@ -6,22 +6,6 @@ from calendar_backend.models import CommentEvent
 
 
 @pytest.fixture()
-def comment_event_path(client_auth: TestClient, dbsession: Session, event_path: str):
-    RESOURCE = f"{event_path}/comment/"
-    request_obj = {
-        "author_name": "Аноним",
-        "text": "Очень умный коммент",
-    }
-    response = client_auth.post(RESOURCE, json=request_obj)
-    id_ = response.json()["id"]
-    client_auth.post(f"{RESOURCE}{id_}/review/", params={"action": "Approved"})
-    yield RESOURCE + str(id_)
-    response_model: CommentEvent = dbsession.query(CommentEvent).get(id_)
-    dbsession.delete(response_model)
-    dbsession.commit()
-
-
-@pytest.fixture()
 def comment_event_path_no_review(client_auth: TestClient, dbsession: Session, event_path: str):
     RESOURCE = f"{event_path}/comment/"
     request_obj = {
@@ -35,21 +19,6 @@ def comment_event_path_no_review(client_auth: TestClient, dbsession: Session, ev
     dbsession.delete(response_model)
     dbsession.commit()
 
-
-@pytest.fixture()
-def comment_event_path_declined_review(client_auth: TestClient, dbsession: Session, event_path: str):
-    RESOURCE = f"{event_path}/comment/"
-    request_obj = {
-        "author_name": "Аноним",
-        "text": "Очень умный коммент",
-    }
-    response = client_auth.post(RESOURCE, json=request_obj)
-    id_ = response.json()["id"]
-    client_auth.post(f"{RESOURCE}{id_}/review/", params={"action": "Declined"})
-    yield RESOURCE + str(id_)
-    response_model: CommentEvent = dbsession.query(CommentEvent).get(id_)
-    dbsession.delete(response_model)
-    dbsession.commit()
 
 
 @pytest.fixture()
@@ -76,23 +45,28 @@ def test_read_all(client_auth: TestClient, comment_event_path_for_read_all: str)
     assert len(response_obj["items"]) <= 10
 
 
-def test_delete(client_auth: TestClient, comment_event_path: str):
-    response = client_auth.delete(comment_event_path)
+def test_delete(client_auth: TestClient, comment_event_path_no_review: str):
+    client_auth.post(f"{comment_event_path_no_review}/review/", params={"action": "Approved"})
+    response = client_auth.delete(comment_event_path_no_review)
     assert response.ok, response.json()
 
 
-def test_patch(client_auth: TestClient, comment_event_path: str, comment_event_path_no_review: str):
+def test_patch(client_auth: TestClient, comment_event_path_no_review: str):
     request = {
         "text": "Не очень умный коммент",
     }
-    response = client_auth.patch(comment_event_path, json=request)
-    assert response.status_code == 403
 
     response = client_auth.patch(comment_event_path_no_review, json=request)
     assert response.ok, response.json()
     assert response.json()["text"] == request["text"]
 
+    client_auth.post(f"{comment_event_path_no_review}/review/", params={"action": "Approved"})
 
-def test_review(client_auth: TestClient, comment_event_path_declined_review):
-    response = client_auth.get(comment_event_path_declined_review)
+    response = client_auth.patch(comment_event_path_no_review, json=request)
+    assert response.status_code == 403
+
+
+def test_review(client_auth: TestClient, comment_event_path_no_review):
+    client_auth.post(f"{comment_event_path_no_review}/review/", params={"action": "Declined"})
+    response = client_auth.get(comment_event_path_no_review)
     assert response.status_code == 404
--- tests/lecturer/comments.py
+++ tests/lecturer/comments.py
@@ -2,40 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-from calendar_backend.exceptions import ForbiddenAction
 from calendar_backend.models import CommentLecturer
-
-
-@pytest.fixture()
-def comment_path(client_auth: TestClient, dbsession: Session, lecturer_path: str):
-    RESOURCE = f"{lecturer_path}/comment/"
-    request_obj = {
-        "author_name": "Аноним",
-        "text": "Очень умный коммент",
-    }
-    response = client_auth.post(RESOURCE, json=request_obj)
-    id_ = response.json()["id"]
-    client_auth.post(f"{RESOURCE}{id_}/review/", params={"action": "Approved"})
-    yield RESOURCE + str(id_)
-    response_model: CommentLecturer = dbsession.query(CommentLecturer).get(id_)
-    dbsession.delete(response_model)
-    dbsession.commit()
-
-
-@pytest.fixture()
-def comment_path_declined_review(client_auth: TestClient, dbsession: Session, lecturer_path: str):
-    RESOURCE = f"{lecturer_path}/comment/"
-    request_obj = {
-        "author_name": "Аноним",
-        "text": "Очень умный коммент",
-    }
-    response = client_auth.post(RESOURCE, json=request_obj)
-    id_ = response.json()["id"]
-    client_auth.post(f"{RESOURCE}{id_}/review/", params={"action": "Declined"})
-    yield RESOURCE + str(id_)
-    response_model: CommentLecturer = dbsession.query(CommentLecturer).get(id_)
-    dbsession.delete(response_model)
-    dbsession.commit()
 
 
 @pytest.fixture()
@@ -76,24 +43,28 @@ def test_read_all(client_auth: TestClient, comment_path_for_read_all: str):
     assert len(response_obj["items"]) <= 10
 
 
-def test_delete(client_auth: TestClient, comment_path: str):
-    response = client_auth.delete(comment_path)
+def test_delete(client_auth: TestClient, comment_path_no_review: str):
+    client_auth.post(f"{comment_path_no_review}/review/", params={"action": "Approved"})
+    response = client_auth.delete(comment_path_no_review)
     assert response.ok, response.json()
 
 
-def test_patch(client_auth: TestClient, comment_path: str, comment_path_no_review: str):
+def test_patch(client_auth: TestClient, comment_path_no_review: str):
     request = {
         "text": "Не очень умный коммент",
     }
-    response = client_auth.patch(comment_path, json=request)
-    assert response.status_code == 403
-
 
     response = client_auth.patch(comment_path_no_review, json=request)
     assert response.ok, response.json()
     assert response.json()["text"] == request["text"]
 
+    client_auth.post(f"{comment_path_no_review}/review/", params={"action": "Approved"})
 
-def test_review(client_auth: TestClient, comment_path_declined_review):
-    response = client_auth.get(comment_path_declined_review)
+    response = client_auth.patch(comment_path_no_review, json=request)
+    assert response.status_code == 403
+
+
+def test_review(client_auth: TestClient, comment_path_no_review: str):
+    client_auth.post(f"{comment_path_no_review}/review/", params={"action": "Declined"})
+    response = client_auth.get(comment_path_no_review)
     assert response.status_code == 404
