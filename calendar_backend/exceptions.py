bb879da5427cc36aec5278168cf24d703e364134
semen603089 <semen603089@mail.ru>
2022-10-16 21:36:11 +0300

    fix 500

--- calendar_backend/exceptions.py
+++ calendar_backend/exceptions.py
@@ -7,7 +7,8 @@ class ObjectNotFound(Exception):
 
 
 class NotEnoughCriteria(Exception):
-    pass
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
 class ForbiddenAction(Exception):
