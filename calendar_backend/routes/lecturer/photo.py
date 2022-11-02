c77304e13baa75aaa812c4c0181c90fdbf7e9414
semen603089 <semen603089@mail.ru>
2022-11-02 12:05:38 +0300

    fix #62

--- calendar_backend/routes/lecturer/photo.py
+++ calendar_backend/routes/lecturer/photo.py
@@ -3,7 +3,7 @@ from fastapi_sqlalchemy import db
 
 from calendar_backend.exceptions import ObjectNotFound
 from calendar_backend.methods import utils
-from calendar_backend.models.db import ApproveStatuses
+from calendar_backend.models.db import ApproveStatuses, Lecturer
 from calendar_backend.models.db import Photo as DbPhoto
 from calendar_backend.routes.models import (
     Photo,
@@ -32,6 +32,8 @@ async def upload_photo(lecturer_id: int, photo: UploadFile = File(...)) -> Photo
 
 @lecturer_photo_router.get("/photo", response_model=LecturerPhotos)
 async def get_lecturer_photos(lecturer_id: int, limit: int = 10, offset: int = 0) -> LecturerPhotos:
+    if not Lecturer.get(id=lecturer_id, session=db.session):
+        raise ObjectNotFound(Lecturer, lecturer_id)
     res = DbPhoto.get_all(session=db.session).filter(DbPhoto.lecturer_id == lecturer_id)
     if limit:
         cnt, res = res.count(), res.offset(offset).limit(limit).all()
@@ -52,6 +54,8 @@ async def delete_photo(id: int, lecturer_id: int) -> None:
 
 @lecturer_photo_router.get("/photo/{id}", response_model=Photo)
 async def get_photo(id: int, lecturer_id: int) -> Photo:
+    if not Lecturer.get(id=lecturer_id, session=db.session):
+        raise ObjectNotFound(Lecturer, lecturer_id)
     photo = DbPhoto.get(id, session=db.session)
     if photo.lecturer_id != lecturer_id or photo.approve_status != ApproveStatuses.APPROVED:
         raise ObjectNotFound(DbPhoto, id)
