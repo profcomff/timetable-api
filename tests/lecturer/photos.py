d13a5de7d4bd7edb98270d42fb3730ba981f4819
Grigoriev Semyon <33061489+grigoriev-semyon@users.noreply.github.com>
2022-12-10 02:45:35 +0300

    File extension checker (#77)

--- tests/lecturer/photos.py
+++ tests/lecturer/photos.py
@@ -1,7 +1,9 @@
+import os
+
 from fastapi.testclient import TestClient
 from starlette import status
-from calendar_backend.settings import get_settings
 
+from calendar_backend.settings import get_settings
 
 settings = get_settings()
 settings.STATIC_PATH = './static'
@@ -25,3 +27,11 @@ def test_delete(client_auth: TestClient, photo_path: str):
 
     response = client_auth.get(photo_path)
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_unsupported_format(lecturer_path: str, client_auth: TestClient):
+    RESOURCE = f"{lecturer_path}/photo"
+    with open(os.path.dirname(__file__) + "/photo.notpng", "rb") as f:
+        response = client_auth.post(RESOURCE, files={"photo": f})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
